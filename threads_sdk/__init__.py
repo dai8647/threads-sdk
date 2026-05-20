87be1fd178ea20ec7e67cbed466f334f1df7bac3
dai8647 <dai8647@gmail.com>
2026-05-20 16:12:09 +0900

    feat: add package exports

--- threads_sdk/__init__.py
+++ threads_sdk/__init__.py
@@ -1,1 +1,42 @@
 """Modern Python SDK for Meta's Threads API."""
+
+from threads_sdk.async_client import AsyncThreadsClient
+from threads_sdk.auth import Auth, Credentials
+from threads_sdk.client import ThreadsClient
+from threads_sdk.exceptions import (
+    AuthenticationError,
+    NotFoundError,
+    RateLimitError,
+    ThreadsError,
+    ValidationError,
+)
+from threads_sdk.models import (
+    ContainerStatus,
+    LongLivedTokenResponse,
+    PostInsights,
+    PostReply,
+    ThreadsPost,
+    TokenResponse,
+    UserProfile,
+)
+
+__all__ = [
+    "AsyncThreadsClient",
+    "Auth",
+    "AuthenticationError",
+    "ContainerStatus",
+    "Credentials",
+    "LongLivedTokenResponse",
+    "NotFoundError",
+    "PostInsights",
+    "PostReply",
+    "RateLimitError",
+    "ThreadsClient",
+    "ThreadsError",
+    "ThreadsPost",
+    "TokenResponse",
+    "UserProfile",
+    "ValidationError",
+]
+
+__version__ = "0.1.0"
